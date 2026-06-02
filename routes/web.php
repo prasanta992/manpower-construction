b9484b0ade8fcaf5884e3655127b4a2487eadb99
Prasanta <prasantasah00me@gmail.com>
2026-06-02 15:14:32 +0530

    2nd

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-
-Route::get('/', [HomeController::class,'index'])->name('home.index');
+Route::get('/', function () {
+    return "OK - Railway working";
+});
+//Route::get('/', [HomeController::class,'index'])->name('home.index');
 Route::get('/service', [HomeController::class, 'service'])->name('service.index');
 Route::get('/projects', [HomeController::class, 'projects'])->name('projects.index');
 Route::get('/testimonials', [HomeController::class, 'testimonials'])->name('testimonials.index');
